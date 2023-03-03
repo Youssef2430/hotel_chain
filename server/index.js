8d457e4c63b1f5d8d9c422382aa4ba073d06af9b
Youssef30 <ychou031@uottawa.ca>
2023-03-03 12:20:40 -0500

    finished APIs exemples and created client side

--- server/index.js
+++ server/index.js
@@ -7,17 +7,60 @@ const pool = require('./db');
 app.use(cors());
 app.use(express.json());
 
-// basic route
+// basic routing
+
+// exemple of a post request
 app.post('/person', async (req, res) => {
 
     try {
-        const { fullname, email } = req.body;
+        const { fullname, gender } = req.body;
         const newPerson = await pool.query(
-            "INSERT INTO person (fullname, email) VALUES($1, $2) RETURNING *",
-            [fullname, email]
+            "INSERT INTO person (fullname, gender) VALUES($1, $2) RETURNING *",
+            [fullname, gender]
         );
         res.json(newPerson.rows[0]);
     } catch (err) {
+        console.error(err.message);
+    }
+});
+
+// exemple of a get request
+app.get('/person', async (req, res) => {
+    try {
+        const allPerson = await pool.query("SELECT * FROM person");
+        res.json(allPerson.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+// exemple of a put request
+app.put('/person/:id', async (req, res) => {
+
+    try {
+        const{id} = req.params;
+        const{fullname, gender} = req.body;
+        const updatePerson = await pool.query(
+            "UPDATE person SET fullname = $1, gender = $2 WHERE SIN = $3",
+            [fullname, gender, id]
+        );
+        res.json("Person was updated!");
+    } catch (error) {
+        console.error(err.message);
+    }
+});
+
+// exemple of a delete request
+app.delete('/person/:id', async (req, res) => {
+
+    try {
+        const{id} = req.params;
+        const deletePerson = await pool.query(
+            "DELETE FROM person WHERE SIN = $1",
+            [id]
+        );
+        res.json("Person was deleted!");
+    } catch (error) {
         console.error(err.message);
     }
 });
